98523279b57b1688cdb00333aaa9319f4d049e7b
thiagoluppi <thiago.luppi@icloud.com>
2021-10-31 09:06:04 -0300

    commit aula_melhorando_codigo_de_testes_page_objects

--- features/step_definitions/login_steps.rb
+++ features/step_definitions/login_steps.rb
@@ -6,22 +6,9 @@ Dado("que acesso a pagina principal") do
 end
 
 Quando("submeto minhas credenciais com {string} e {string}") do |email, senha|
-  # Aqui chamamos o passo "Dado que acesso a pagina de cadastro" e logo em seguida, usamos %{} para fazer uma string multiline e passar a datatable,
-  # no passo "Quando submeto o seguinte formulario de cadastro:" e por fim usamos o passo "Dado que acesso a pagina principal" para voltar a pagina principal:
-
-  # isso não foi feito pelo professor, foi feito apenas para que o cenario nao quebre, ao rodar todos os testes num regressivo.
-
-  steps %{
-    Dado que acesso a pagina de cadastro
-    Quando submeto o seguinte formulario de cadastro:
-      | nome                 | email                   | senha  |
-      | Thiago Messias Luppi | thiago.luppi@icloud.com | pwd123 |
-    Dado que acesso a pagina principal
-  }
-
-  find("input[placeholder='Seu email']").set email
-  # aqui não precisaria colocar password entre aspas porque se trata de uma palavra. Se fossem duas, como acima, seria necessario.
-  find("input[type='password']").set senha
-
-  click_button "Entrar"
+  login_page = LoginPage.new
+  login_page.abrir
+  login_page.campo_email.set email
+  login_page.campo_senha.set senha
+  login_page.botao_entrar
 end
